e5207c03e00503fdbb2502ef04ade607da3f516f
Chandher <chandher@blocktheory.com>
2025-07-10 23:45:21 +0530

    fix: added to path

--- Formula/nexio.rb
+++ Formula/nexio.rb
@@ -5,7 +5,7 @@ class Nexio < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nexioxyz/homebrew-nexio/releases/download/v7.5.0/nexio-macos-arm64.tar.gz"
+      url "https://github.com/nexioxyz/homebrew-nexio/nexio-macos-arm64.tar.gz"
       sha256 "1b0a6269e91d3adddd78c2ffaa3975f79d3230d0f4679ea51bc0f62d032ef697"
     end
     if Hardware::CPU.intel?
