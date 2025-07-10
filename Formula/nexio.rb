3519bff9004a4d4d41bebbde28593a2d6be1beb7
Chandher <chandher@blocktheory.com>
2025-07-10 23:38:23 +0530

    added brew updates for repo

--- Formula/nexio.rb
+++ Formula/nexio.rb
@@ -1,15 +1,15 @@
 class Nexio < Formula
-  desc "Nexio - Aptos CLI tool"
-  homepage "https://github.com/yourusername/nexio"
+  desc "Nexio - CLI tool"
+  homepage "https://github.com/nexioxyz/nexio"
   version "7.5.0"
-  
+
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yourusername/homebrew-nexio/releases/download/v7.5.0/nexio-macos-arm64.tar.gz"
+      url "https://github.com/nexioxyz/homebrew-nexio/releases/download/v7.5.0/nexio-macos-arm64.tar.gz"
       sha256 "1b0a6269e91d3adddd78c2ffaa3975f79d3230d0f4679ea51bc0f62d032ef697"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/yourusername/homebrew-nexio/releases/download/v7.5.0/nexio-macos-x86_64.tar.gz"
+      url "https://github.com/nexioxyz/homebrew-nexio/releases/download/v7.5.0/nexio-macos-x86_64.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256"
     end
   end
