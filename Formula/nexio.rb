818762125d7d82c69a6e7be1e60ad556ed29a2b9
Chandher <chandher@blocktheory.com>
2025-07-16 13:16:42 +0530

    updating the brew formula

--- Formula/nexio.rb
+++ Formula/nexio.rb
@@ -7,10 +7,9 @@ class Nexio < Formula
     if Hardware::CPU.arm?
       url "https://github.com/nexioxyz/homebrew-nexio/releases/download/testnet/nexio-macos-arm64.tar.gz"
       sha256 "f86b39df3c3d77a04757aacf66a82b0215a4d4a1745df47932fe04ecd0df1e8c"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/nexioxyz/homebrew-nexio/releases/download/testnet/nexio-macos-arm64.tar.gz"
-      sha256 "f86b39df3c3d77a04757aacf66a82b0215a4d4a1745df47932fe04ecd0df1e8c"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/nexioxyz/homebrew-nexio/releases/download/testnet/nexio-macos-amd64.tar.gz"
+      sha256 "<intel‑sha256‑here>"
     end
   end
 
@@ -18,7 +17,13 @@ class Nexio < Formula
     bin.install "nexio"
   end
 
+  def post_install
+    # Remove the quarantine flag so Gatekeeper won’t warn on first launch
+    system "xattr", "-dr", "com.apple.quarantine", bin/"nexio"
+  end
+
   test do
-    system "#{bin}/nexio", "--version"
+    assert_match version.to_s, shell_output("#{bin}/nexio --version")
   end
 end
+
